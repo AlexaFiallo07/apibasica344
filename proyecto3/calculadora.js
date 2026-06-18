dfab0cf2be5313f059ef67e5ab0e070041698603
alexandra <alexafialloc@gmail.com>
2026-06-18 12:33:18 -0500

    agregar archivos

--- proyecto3/calculadora.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk');
-
-function sumar(a, b){
-    return a + b;
-
-}
-
-console.log(chalk.green('Resultado:'), sumar(2, 3));
--- proyecto4/otrosmodulos.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const http = require('http');
-const path = require('path');
-
-console.log('Directorio actual:', __dirname);
-console.log('Archivos en esta carpeta:', fs.readdirSync('.'));
-
-const colors = require('colors');
-console.log('hola'.rainbow);
