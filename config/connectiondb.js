f57c64aa80460a2e3fdc7d8e097f3600ec63732b
alexa <alexafialloc@gmail.com>
2026-06-29 20:19:47 -0500

    cambio de .env

--- config/connectiondb.js
+++ config/connectiondb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const URI = process.env.MongoDB_URI + process.env.dbName;
+const URI = process.env.MONGODB_URI;
 const connectDB = async () => {
   try {
     await mongoose.connect(URI);
