81b8fcb75530532fb35b50e1ed8d68dce36bb4ba
alexa <alexafialloc@gmail.com>
2026-06-29 20:08:16 -0500

    1

--- config/connectiondb.js
+++ config/connectiondb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const URI = process.env.MongoDB_URI;
+const URI = process.env.MongoDB_URI + process.env.dbName;
 const connectDB = async () => {
   try {
     await mongoose.connect(URI);
